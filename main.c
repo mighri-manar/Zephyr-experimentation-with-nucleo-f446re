e13b3642e95a1760970cf7b123384e367adb19a6
Manar Mighri <144473914+mighri-manar@users.noreply.github.com>
2024-12-24 03:17:43 +0100

    Add files via upload
    this app configuers a pwm timer to generate signals with a constant duty and a dynamic frequency that varies between 500 and 2000 with a step of 50

--- main.c
+++ main.c
@@ -1,22 +1,22 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/pwm.h>
 #include <zephyr/logging/log.h>
-#include <math.h> // For sine function
 
 LOG_MODULE_REGISTER(main);
 
 #define PWM_NODE DT_NODELABEL(pwm2)
 #define PWM_CHANNEL 1
-#define PWM_PERIOD_USEC 20000 
-#define RESOLUTION 1000      
-
-
-#define PI 3.141592653589793
+#define INITIAL_FREQ_HZ 500    
+#define MAX_FREQ_HZ 2000        
+#define MIN_FREQ_HZ 500         
+#define DUTY_CYCLE_PERCENT 50   
 
 void main(void)
 {
     const struct device *pwm_dev;
-    uint32_t pulse_width;
+    uint32_t freq = INITIAL_FREQ_HZ;
+    int step = 50; 
+    uint32_t pulse_width, period_usec;
 
     pwm_dev = DEVICE_DT_GET(PWM_NODE);
     if (!device_is_ready(pwm_dev)) {
@@ -28,17 +28,24 @@ void main(void)
 
     while (1) {
         
-        for (int i = 0; i < 360; i += 10) {
-            float sine_value = (sin(i * PI / 180.0) + 1) / 2.0; //normalisation
-            pulse_width = sine_value * PWM_PERIOD_USEC;          
+        period_usec = (1000000U / freq);     // Period = 1/freq in microseconds
 
-            if (pwm_set(pwm_dev, PWM_CHANNEL, PWM_PERIOD_USEC, pulse_width, 0)) {
-                LOG_ERR("Failed to set PWM");
-            } else {
-                LOG_INF("PWM duty cycle set: %d%%", (int)((pulse_width * 100) / PWM_PERIOD_USEC));
-            }
+        
+        pulse_width = (period_usec * DUTY_CYCLE_PERCENT) / 100;
 
-            k_sleep(K_MSEC(5)); 
+        
+        if (pwm_set(pwm_dev, PWM_CHANNEL, period_usec, pulse_width, 0)) {
+            LOG_ERR("Failed to set PWM");
+        } else {
+            LOG_INF("Frequency: %d Hz, Duty Cycle: %d%%", freq, DUTY_CYCLE_PERCENT);
+        }
+
+        k_sleep(K_MSEC(100)); 
+
+        
+        freq += step;
+        if (freq > MAX_FREQ_HZ || freq < MIN_FREQ_HZ) {
+            step = -step; 
         }
     }
 }
